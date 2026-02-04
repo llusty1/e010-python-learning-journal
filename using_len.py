f73c5db3941561001b359d58d824bfd8e09cbd45
llusty1 <lawrencelusty@proton.me>
2026-02-04 11:27:36 -0600

    Commit

---
+++ using_len.py
@@ -0,0 +1,12 @@
+# This program is to practice using the len function in Python
+
+george_v = "His Makesty George V, by the Grace of God, " \
+            "of the United Kingdom and Great Britain and, " \
+            "Ireland and the British Dominions beyond, " \
+            "the Seas, King, Defender of the Faith, Emperor of India."
+gandhi = "Monandas Karamchad Gandhi"
+john_f_kennedy = "JFK"
+
+print(len(george_v), 'characters is too long for a name!')
+print(len(gandhi), 'is much better...')
+print(len(john_f_kennedy), 'characters is short enough.')
