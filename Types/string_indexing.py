e818d1646565df876169032500f87510038f54d1
llusty1 <lawrencelusty@proton.me>
2026-02-04 11:55:26 -0600

    Commit

---
+++ Types/string_indexing.py
@@ -0,0 +1,19 @@
+# This is to practice string indexing in Python
+
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+print(
+    alphabet[0],
+    alphabet[19],
+    alphabet[19],
+    alphabet[0],
+    alphabet[2],
+    alphabet[10]
+)
+print(
+    alphabet[2],
+    alphabet[7],
+    alphabet[8],
+    alphabet[13],
+    alphabet[0]
+)
