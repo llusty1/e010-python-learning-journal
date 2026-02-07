bde37f579b00fbbd1650d9fd0eec21bfae315955
llusty1 <lawrencelusty@proton.me>
2026-02-06 21:47:14 -0600

    Commit

---
+++ Types/my_list.py
@@ -0,0 +1,15 @@
+# Adding and removing list elements
+my_list = [10, "bw"]
+print(f'My list: {my_list}')
+
+# Let's append something here
+my_list.append("abc")
+print(f'After append: {my_list}')
+
+# Now let's pop something here
+my_list.pop(1)
+print(f'After pop: {my_list}')
+
+# Now let's remove something here
+my_list.remove("abc")
+print(f'After remove: {my_list}')
