10fd9cbc6553a1a160f2ee06d337b2a9a196877d
llusty1 <lawrencelusty@proton.me>
2026-02-06 21:22:37 -0600

    Commit

---
+++ Types/prices.py
@@ -0,0 +1,7 @@
+# This script shows how you can update a list
+prices = [1.50, 3.75, 6.00]
+print("Cost: ", prices[0] + prices[1])
+
+# Update the first price in the list
+prices[0] = 2.50
+print("Updated cost: ", prices[0] + prices[1])
