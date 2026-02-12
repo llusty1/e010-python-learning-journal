4ca404d1996c179b2e675b67eb403e059adebf04
llusty1 <lawrencelusty@proton.me>
2026-02-11 22:33:14 -0600

    Commit

---
+++ Types/using_tuples.py
@@ -0,0 +1,9 @@
+# This is where we will learn about tuples!
+white_house_coordinates = (38.8977, 77.0366)
+print()
+print(f'Coordinates: {white_house_coordinates}')
+print(f'Tuple length: {len(white_house_coordinates)}')
+
+# Access tuples via index
+print(f'\nLatitude: {white_house_coordinates[0]} north')
+print(f'Longitude: {white_house_coordinates[1]} west\n')
