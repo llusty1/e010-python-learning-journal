a63da3cfc7ae022b6be06a24bdd7032fdfa6a719
llusty1 <lawrencelusty@proton.me>
2026-02-13 18:45:28 -0600

    Commit

---
+++ Types/creating_sets.py
@@ -0,0 +1,10 @@
+# Create a set using the set function. A set function = set()
+nums1 = set([1, 2, 3, 4, "cretins wanna hop some more."])
+
+# Create a set using a set literal. A set literal uses curly braces {}
+nums2 = {4, 5, 6, 7, "all the cretins go to heaven."}
+
+# Print the contents of the sets:
+
+print(nums1)
+print(nums2)
