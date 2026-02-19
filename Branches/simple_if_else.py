6cd9f5f2318fc40fa4ade7d6ccb1b1d3c81e39d5
llusty1 <lawrencelusty@proton.me>
2026-02-18 18:59:52 -0600

    Commit

---
+++ Branches/simple_if_else.py
@@ -0,0 +1,6 @@
+# Here I will create a simple program that asks a question.
+answer = input("You are a man or a woman?")
+if answer.lower() == " man":
+    print("What's up dude?!?")
+else:
+    print("Well hello there...what have we here?")
