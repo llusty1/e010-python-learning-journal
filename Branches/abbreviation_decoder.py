3af1df098bba63244e437b3b2c0fc240659b6601
llusty1 <lawrencelusty@proton.me>
2026-02-26 19:20:51 -0600

    Commit

---
+++ Branches/abbreviation_decoder.py
@@ -0,0 +1,14 @@
+# Here we will practice skills learned in the Branches module.
+
+message = input('Enter abbreviation:\n')
+
+if message == 'LOL':
+    print('LOL = laughing out loud')
+elif message == 'BFN':
+    print('BFN = bye for now')
+elif message == 'FTW':
+    print('FTW = for the win')
+elif message == 'IRL':
+    print('IRL = in real life')
+else:
+    print("Sorry, don't know that one")
