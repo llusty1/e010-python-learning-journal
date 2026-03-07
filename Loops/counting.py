f846e91423f711541a958087837a3ce983c1531d
llusty1 <lawrencelusty@proton.me>
2026-03-07 15:24:28 -0600

    Commit

---
+++ Loops/counting.py
@@ -0,0 +1,35 @@
+i = 1
+while i <= 2:
+    print(i)
+    i += 1
+    
+print("-" * 15) # Line break here
+
+i = 1
+while i <= 2:
+    i += 1
+    print(i)
+
+print("-" * 15)
+
+i = 0 
+while i <= 100:
+    i += 2
+    print(i)
+    
+print("-" * 15)
+
+i = 10
+while i >= 1:
+    print(i)
+    i -= 1
+
+print("Blast off!!!")
+print()
+
+i = -100
+while i <= 65:
+    print(i)
+    i += 1
+print()
+
