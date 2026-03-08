eb32362fdf9bffa7d956e44ef88767c53ec7815d
llusty1 <lawrencelusty@proton.me>
2026-03-08 18:40:10 -0500

    Commit

---
+++ Loops/break_statement.py
@@ -0,0 +1,28 @@
+# A break statement in a loop causes the loop to exit immediately. 
+# A break statement can sometimes yield a loop that is easier to understand.
+
+empanada_cost = 3
+taco_cost = 4
+
+user_money = int(input('Enter money for meal: '))
+
+max_empanadas = user_money // empanada_cost
+max_tacos = user_money // taco_cost
+
+meal_cost = 0
+for num_tacos in range(max_tacos + 1):
+    for num_empanadas in range(max_empanadas + 1):
+        meal_cost = (num_empanadas * empanada_cost) + (num_tacos * taco_cost)
+
+        # Find first meal option that exactly matches user money
+        if meal_cost == user_money:
+            break
+
+    # Find first meal option that exactly matches user money
+    if meal_cost == user_money:
+        break
+
+if meal_cost == user_money:
+    print(f'${meal_cost} buys {num_empanadas} empanadas and {num_tacos} tacos without change.')
+else:
+    print('You cannot buy a meal without having change left over.')
