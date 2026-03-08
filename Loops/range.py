a7f7905fe025c0da86ca265b80caf247ecebbaa7
llusty1 <lawrencelusty@proton.me>
2026-03-08 17:49:12 -0500

    Commit

---
+++ Loops/range.py
@@ -0,0 +1,20 @@
+# Let's do a few range problems and run them in the terminal 
+# In Python, the range() function generates a sequence of integers and is primarily 
+# used to control the number of iterations in a for loop. 
+# It is technically an immutable sequence type (or class) rather than a simple function.
+
+for i in range(0, 7, 2): # This range() function asks for i from 0-7 by 2.  
+    print(i)
+print()
+
+for i in range(0, -11, -1): # This range() function show how to count backwards by -1
+    print(i)
+print()
+
+for i in range(10, 0, -1):
+    print(i)
+    
+print(f"Happy New Year!!!") # I was trying to figure this out, be careful where you choose 
+                            # to nest your code.
+print()
+
